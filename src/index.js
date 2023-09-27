fd1f7eb4c8ca4599e64f53c33d38c41b2d54ed4e
Cheder-one <cherbadenis1@gmail.com>
2023-09-27 21:01:50 +0300

    Линтеры настроены

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import "./index.html";
 import "./index.scss";
 
 import "./app/components/l-main-container/l-main-container.js";
+
+// TODO Реализация блока "Ремонт различных видов техники"
+
+// TODO Реализовать затуманивание бэкграунда
