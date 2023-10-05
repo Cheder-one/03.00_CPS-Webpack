6cf3c85f0ac804a9aae0aca8da2b2b55f510a3c4
Cheder-one <cherbadenis1@gmail.com>
2023-10-05 17:35:07 +0300

    Внесены корректировки

--- src/index.js
+++ src/index.js
@@ -3,8 +3,3 @@ import "./index.scss";
 
 import "./app/components/main-container/main-container.js";
 import "./app/components/aside-container/aside-container.js";
-
-// TODO Реализовать "Обратная связь"
-// TODO Реализовать "Заказать звонок"
-// TODO Реализовать "Читать далее"
-// TODO Реализовать "menu-list__item--active"
